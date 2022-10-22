a0ad76d571db3adc4c4f6b30d501bfc0568922f8
Zong Wei Eng <engzongwei@hotmail.com>
2022-10-22 15:03:04 +0800

    Updated router to always bring to top

--- src/router/index.js
+++ src/router/index.js
@@ -57,7 +57,11 @@ const routes = [
 
 const router = createRouter({
     history: createWebHistory(process.env.BASE_URL),
-    routes
+    routes,
+    scrollBehavior(to, from, savedPosition) {
+        // always scroll to top
+        return { top: 0 }
+      },
 })
 
 export default router
