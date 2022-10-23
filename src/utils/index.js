eb0216a265d399c730126887abf01248e777447b
Alethea Toh <aletheatohh@gmail.com>
2022-10-23 14:23:03 +0800

    dependency added

--- src/utils/index.js
+++ src/utils/index.js
@@ -8,14 +8,14 @@ import { initializeApp } from 'firebase/app';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAIIHmCjATac_3pg0M8gVHs2wN0h_I7GN4",
-  authDomain: "wad2project-d5b31.firebaseapp.com",
-  databaseURL: "https://wad2project-d5b31-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "wad2project-d5b31",
-  storageBucket: "wad2project-d5b31.appspot.com",
-  messagingSenderId: "457383848533",
-  appId: "1:457383848533:web:bce9727937c8d714bfc2dc",
-  measurementId: "G-JFFFZGKRGY"
+    apiKey: process.env.VUE_APP_API_KEY,
+    authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+    databaseURL: process.env.VUE_APP_DATABASE_URL,
+    projectId: process.env.VUE_APP_PROJECT_ID,
+    storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+    appId: process.env.VUE_APP_APP_ID,
+    measurementId: process.env.VUE_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
