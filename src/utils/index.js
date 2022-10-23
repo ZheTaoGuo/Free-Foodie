18ab0eebab7bef4bfc76af3974b4b1370af9b11a
Zong Wei Eng <engzongwei@hotmail.com>
2022-10-23 13:41:51 +0800

    Update index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -9,13 +9,13 @@ import { initializeApp } from 'firebase/app';
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.VUE_APP_API_KEY,
-  authDomain: VUE_APP_AUTH_DOMAIN,
-  databaseURL: VUE_APP_DATABASE_URL,
-  projectId: VUE_APP_PROJECT_ID,
-  storageBucket: VUE_APP_STORAGE_BUCKET,
-  messagingSenderId: VUE_APP_MESSAGING_SENDER_ID,
-  appId: VUE_APP_APP_ID,
-  measurementId: VUE_APP_MEASUREMENT_ID
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_DATABASE_URL,
+  projectId: process.env.VUE_APP_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_APP_ID,
+  measurementId: process.env.VUE_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
