0524116b8d59f6ef60d4ef0b3934ed54720d858a
Zong Wei Eng <engzongwei@hotmail.com>
2022-10-27 17:51:57 +0800

    Update IngredientView.vue

--- src/components/IngredientView.vue
+++ src/components/IngredientView.vue
@@ -26,7 +26,7 @@
         <li class="list-group-item" v-for="item of ingredients">
             <span v-if="Number.isInteger(item.amount)">{{item.amount}}&nbsp</span> 
             <span v-else>{{item.amount.toFixed(2)}}&nbsp</span>
-            <span v-if="item.unit">{{item.unit}} of</span>{{item.originalName}}
+            <span v-if="item.unit">{{item.unit}} of </span>{{item.originalName}}
         </li>
     </ul>
 </template>
