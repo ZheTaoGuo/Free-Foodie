5c4c8fd352578a4fc9dc02f257a9c2ac08151ed0
Alethea Toh <aletheatohh@gmail.com>
2022-11-12 17:03:27 +0800

    Implemented mobile view for fridge

---
+++ src/views/ShoppingListInit.vue
@@ -0,0 +1,27 @@
+<template>
+</template>
+<script>
+    export default {
+        name: 'Init',
+        methods: {
+            isMobile() {
+                if( screen.width <= 760 ) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+        },
+        created() {
+            if (this.isMobile()) {
+                this.$router.push('/shoppinglistmobile');
+            }
+            else {
+                this.$router.push('/shoppinglist');
+            }
+        }
+    }
+</script>
+<style scoped>
+</style>
