139a18fec5a95d8c553bcb80bae45b302ff0557c
Zong Wei Eng <engzongwei@hotmail.com>
2022-11-12 19:08:37 +0800

    Updated mobile view of RecipeView

---
+++ src/views/RecipeInit.vue
@@ -0,0 +1,30 @@
+<template>
+</template>
+<script>
+    export default {
+        name: 'Init',
+        methods: {
+            isMobile() {
+                if( screen.width <= 760 ) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+        },
+        created() {
+            console.log('in recipe Init');
+            let queryType = this.$route.query.type;
+            let selectedRecipe = this.$route.query.recipeId;
+            if (this.isMobile()) {
+                this.$router.push({path: '/recipemobile', query: {type: queryType, recipeId: selectedRecipe} });
+            }
+            else {
+                this.$router.push({path: '/recipeweb', query: {type: queryType, recipeId: selectedRecipe} });
+            }
+        }
+    }
+</script>
+<style scoped>
+</style>
