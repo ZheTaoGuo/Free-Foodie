59dc912805ef0461946af4068d960d1d791e3140
Alethea Toh <aletheatohh@gmail.com>
2022-11-13 00:26:08 +0800

    profile page landing mobile

---
+++ src/views/profileLandingPageInit.vue
@@ -0,0 +1,27 @@
+<template>
+</template>
+<script>
+    export default {
+        name: 'ProfileInit',
+        methods: {
+            isMobile() {
+                if( screen.width <= 760 ) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+        },
+        created() {
+            if (this.isMobile()) {
+                this.$router.push('/profilemobile');
+            }
+            else {
+                this.$router.push('/profile');
+            }
+        }
+    }
+</script>
+<style scoped>
+</style>
