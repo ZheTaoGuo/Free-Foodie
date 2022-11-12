028545a9e008ad86bc348e2d00eeb67e87fcc83b
Zong Wei Eng <engzongwei@hotmail.com>
2022-11-13 01:43:03 +0800

    updated signup validation

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -15,9 +15,64 @@
 
     export default {
         name: 'appSignup',
+        data() {
+            return {
+                emailInvalid: false,
+                passwordInvalid: false,
+                nameInvalid: false,
+                ageInvalid: false,
+                genderInvalid: false,
+                useremail: '',
+                userpass: '',
+                name: '',
+                age: '',
+                gender: 'Gender',
+            }
+        },
         methods: {
             register,
-            googlesignup
+            googlesignup,
+            checkFields() {
+
+                if (this.name.length == 0){
+                    this.nameInvalid = true
+                }
+
+                if (this.age.length == 0){
+                    this.ageInvalid = true
+                }
+
+                if (this.gender == 'Gender'){
+                    this.genderInvalid = true
+                }
+
+                if (!this.genderInvalid && !this.ageInvalid && !this.nameInvalid && !this.emailInvalid && !this.passwordInvalid){
+                    this.register()
+                }
+            },
+        },
+        computed: {
+            checkEmail() {
+                if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(this.useremail)) {
+                    this.emailInvalid = true
+                } else{
+                    this.emailInvalid = false
+                }
+            },
+
+            checkPassword() {
+                if (this.userpass.length < 8) {
+                    this.passwordInvalid = true
+                } else if (!/[A-Z]/.test(this.userpass) || !/[a-z]/.test(this.userpass)) {
+                    this.passwordInvalid = true
+                } else if (!/[^0-9a-zA-Z]/.test(this.userpass)) {
+                    this.passwordInvalid = true
+                } else if (!/[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/.test(this.userpass)) {
+                    this.passwordInvalid = true
+                } else {
+                    this.passwordInvalid = false
+                }
+            }
         }
     }
 </script>
@@ -35,21 +90,30 @@
                         <!-- !!! -->
 
                         <div class="form-floating mx-auto mb-3 col-6">
-                            <input class="form-control" id="name" placeholder="Name">
+                            <input class="form-control" id="name" placeholder="Name" v-model="name">
                             <label for="name">Name</label>
+                            <div id="nameHelpBlock" class="form-text text-danger" v-if="nameInvalid">
+                                Name field cannot be left empty
+                            </div>
                         </div>
                         <div class="form-floating mx-auto mb-3 col-6">
                             <input type="number" class="form-control" id="age" placeholder="Input your age" min="1"
-                                max="99">
+                                max="99" v-model="age">
                             <label for="age">Age</label>
+                            <div id="ageHelpBlock" class="form-text text-danger" v-if="ageInvalid">
+                                Age field cannot be left empty
+                            </div>
                         </div>
 
                         <div class="form-floating mx-auto mb-3 col-6">
-                            <select class="form-select" id="gender">
+                            <select class="form-select" id="gender" v-model="gender">
                                 <option selected disabled>Gender</option>
                                 <option value="female">Female</option>
                                 <option value="male">Male</option>
                             </select>
+                            <div id="genderHelpBlock" class="form-text text-danger" v-if="genderInvalid">
+                                Gender field cannot be left empty
+                            </div>
                         </div>
 
                         <div class="form-floating mx-auto mb-3 col-6">
@@ -69,17 +133,24 @@
                             <!-- <p class="h5 mb-3 section-titles">Or create account with email</p> -->
                             <div class="form-floating mx-auto mb-3 col-6">
                                 <input type="email" th:field class="form-control" id="email"
-                                    placeholder="name@example.com">
+                                    placeholder="name@example.com" :onkeypress="checkEmail" v-model="useremail">
                                 <label for="email">Email address</label>
+                                <div id="emailHelpBlock" class="form-text text-danger" v-if="emailInvalid">
+                                    Email address is invalid! Please enter a valid email address
+                                </div>
                             </div>
                             <div class="form-floating mx-auto mb-3 col-6">
                                 <input type="password" th:field="*{password}" class="form-control" id="password"
-                                    placeholder="Password">
+                                    placeholder="Password" :onkeypress="checkPassword" v-model="userpass">
                                 <label for="password">Password</label>
+                                <div id="passwordHelpBlock" class="form-text text-danger" v-if="passwordInvalid">
+                                    Your password must be 8 characters or longer, contain letters and numbers, and must
+                                    not contain spaces, special characters, or emoji.
+                                </div>
                             </div>
                             <button
                                 class="btn btn-outline-success btn-login text-uppercase fw-bold signin-btn m-1 col-6"
-                                @click="register()">Sign up</button>
+                                @click="checkFields()">Sign up</button>
 
                             <div class="my-3">
                                 <button class="btn btn-danger m-1 col-6" id="googleSignIn" @click="googlesignup()">
