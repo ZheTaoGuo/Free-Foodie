535645f1daa894b569d2ac3681f1ab1ce17c7ed2
Alethea Toh <aletheatohh@gmail.com>
2022-11-13 21:19:54 +0800

    Revert "Revert "Merge branch 'master' into ale""
    This reverts commit 8305de6250f481439ed89735c874889833652b91.

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import 'bootstrap/dist/js/bootstrap.min.js';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
-// import PrimeVue from "primevue/config";
+
 
 const app = createApp(App);
 app.use(router);
