4242ec5118e919e13bf1faa1b76eab6d065b4554
Zong Wei Eng <engzongwei@hotmail.com>
2022-11-13 14:31:40 +0800

    Updated Footer in Fav/Past

--- src/views/FavouriteRecipe.vue
+++ src/views/FavouriteRecipe.vue
@@ -59,7 +59,7 @@
         <h1 class="text-center">Favourite Recipes</h1>
     </div>
 
-    <div class="container-fluid">
+    <div class="container-fluid" style="min-height: 70vh;">
 
         <div class="row">
             <!--Start of Search Bar-->
@@ -93,9 +93,8 @@
             <!-- <Card :image_url="'tester'" :name="'fake'"></Card> -->
             <!-- End of Recipe -->
         </div>
-
     </div>
-    <Footer style="position:fixed; left:0; bottom:0; width: 100%;"></Footer>
+    <footer><Footer ></Footer></footer>
 </template>
 
 <style>
